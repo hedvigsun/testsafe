85e4ac29e24cfeeb2f4548198522903781b24750
Hedvig <60605880+hedvigsun@users.noreply.github.com>
2023-10-10 11:46:05 +0200

    Update _main.js
    comment more

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -3,6 +3,36 @@
    ========================================================================== */
 
 $(document).ready(function(){
+// comment more
+var showChar = 100;
+var ellipsestext = "...";
+var moretext = "more";
+var lesstext = "less";
+$('.more').each(function() {
+   var content = $(this).html();
+   
+   if(content.length > showChar) {
+      var c = content.substr(0, showChar);
+      var h = content.substr(showChar-1, content.length - showChar);
+      var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+      
+      $(this).html(html);
+   }
+});
+
+	$(".morelink").click(function(){
+		if($(this).hasClass("less")) {
+			$(this).removeClass("less");
+			$(this).html(moretext);
+		} else {
+			$(this).addClass("less");
+			$(this).html(lesstext);
+		}
+		$(this).parent().prev().toggle();
+		$(this).prev().toggle();
+		return false;
+	});
+
    // Sticky footer
   var bumpIt = function() {
       $("body").css("margin-bottom", $(".page__footer").outerHeight(true));
