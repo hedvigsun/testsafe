f9b21e3b91dc2c7a8268ffc28480bf1ee5b35034
Hedvig <60605880+hedvigsun@users.noreply.github.com>
2023-10-10 13:08:40 +0200

    Update _main.js
    add function readMore

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -104,5 +104,19 @@ $(document).ready(function(){
     closeOnContentClick: true,
     midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
   });
+function readMore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
 
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
 });
